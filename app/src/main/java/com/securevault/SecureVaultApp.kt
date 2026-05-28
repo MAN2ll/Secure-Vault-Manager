9c306fabca55c48a3a63e1c6270d5ecceeea5a24
MAN2ll <a.n.moiseev89@gmail.com>
2026-05-28 12:49:27 +0400

    Update SecureVaultApp.kt

--- app/src/main/java/com/securevault/SecureVaultApp.kt
+++ app/src/main/java/com/securevault/SecureVaultApp.kt
@@ -1,7 +1,28 @@
-package com.securevault
+package com.securevault.ui
 
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.securevault.ui.screens.LockScreen
+import com.securevault.ui.screens.VaultListScreen
 
-@HiltAndroidApp
-class SecureVaultApp : Application()
+@Composable
+fun SecureVaultApp() {
+    val navController = rememberNavController()
+    
+    NavHost(navController, startDestination = "lock") {
+        composable("lock") {
+            LockScreen(
+                onUnlock = { navController.navigate("vault") },
+                onSetup = { navController.navigate("vault") }
+            )
+        }
+        composable("vault") {
+            VaultListScreen(
+                onBack = { navController.popBackStack() },
+                onEntryClick = { /* TODO */ }
+            )
+        }
+    }
+}
